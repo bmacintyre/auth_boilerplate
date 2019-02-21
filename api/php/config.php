985b2adce75a0e9374b66a9ba3f35be5a6b38d3e
Unknown <studiosiris@gmail.com>
2019-02-21 14:03:35 -0500

    toggled to local connect

--- api/php/config.php
+++ api/php/config.php
@@ -2,25 +2,12 @@
 
 
 
-   // define('DB_SERVER', 'localhost:3306');
-   // define('DB_USERNAME', 'root');
-   // define('DB_PASSWORD', '');
-   // define('DB_DATABASE', 'py_api');
-
-   define('DB_SERVER', '127.0.0.1:3306');
-   define('DB_USERNAME', 'admin');
-   define('DB_PASSWORD', 'Sliteleemadd7!@#$');
-   define('DB_DATABASE', 'boilerplate');
+   define('DB_SERVER', 'localhost:3306');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'py_api');
 
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   // if($db)
-   // {
-   //     echo "OK";
-   // }
-   // else
-   // {
-   //     echo "Error";
-   // }
 
    define('CLIENT_AUTHORITY', 'brucemacintyre.com');
    define('CLIENT_ID', '4a2fa3b4-c160-4436-82d3-148f602c9aa8');
